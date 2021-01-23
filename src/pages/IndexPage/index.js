7e84757eb22e220ec661d48d450e391161bb4ea2
Petro Hyppönen <petro.hypponen@edu.turkuamk.fi>
2021-01-23 16:55:39 +0200

    Made styles of IndexPage and UploadPage more consistent with each other and added some mobile compatibility.

--- src/pages/IndexPage/index.js
+++ src/pages/IndexPage/index.js
@@ -1,27 +1,22 @@
 import { Link } from 'react-router-dom';
 import './IndexStyle.scss';
 import MusicLibrary from './MusicLibrary';
+import Header from '../../components/Header';
 
-const IndexPage = () => {
-  return (
-    <div className="IndexWrapper">
-      <header>
-        <Link to="/lisaa">Musanlähetyssivulle</Link>
-      </header>
-      <div className="IndexContent" >
-        <img src="leima.svg" alt="Turun Wappuradio" width="300" height="300"/>
-        <h1>Musiikkikirjaston etusivu</h1>
-        <p>Onnittelut, löysit Turun Wappuradion musiikkikirjaston!
-          Tältä sivulta löydät kaikki musiikkikirjastoon hyväksytyt kapplaeet.
-          Jos etsimääsi kappaletta ei ole kirjastossa, 
-          voit hankkia sen laillisin keinoin esimerkiksi rippaamalla CD-levyltä ja lisätä sen kirjastoon
-          {' '}<Link to="/lisaa">tältä sivulta löytyvällä lomakkeella</Link>.
-          Lisätietoja Turun Wappuradion musiikkikäytännöistä löydät{' '}
-          <a target="_blank" rel="noreferrer" href="https://www.turunwappuradio.com/musiikki">täältä</a>.</p>
-      </div>
-      <MusicLibrary />
+const IndexPage = () => (
+  <>
+    <Header title="Musiikkikirjasto"/>
+    <div className="IndexContent">
+      <p>Onnittelut, löysit Turun Wappuradion musiikkikirjaston!
+        Tältä sivulta löydät kaikki musiikkikirjastoon hyväksytyt kappaleet.
+        Jos etsimääsi kappaletta ei ole kirjastossa, 
+        voit hankkia sen laillisin keinoin esimerkiksi rippaamalla CD-levyltä ja lisätä sen kirjastoon
+        {' '}<Link to="/lisaa">tältä sivulta löytyvällä lomakkeella</Link>.
+        Lisätietoja Turun Wappuradion musiikkikäytännöistä löydät{' '}
+        <a target="_blank" rel="noreferrer" href="https://www.turunwappuradio.com/musiikki">täältä</a>.</p>
     </div>
-  );
-}
+    <MusicLibrary />
+  </>
+);
 
 export default IndexPage;
