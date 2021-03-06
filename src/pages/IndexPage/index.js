c7b83fc34842c041c6131017421c9438c54909ad
Arttu Huttunen <huttunen.arttu@gmail.com>
2021-03-06 22:18:36 +0200

    Bugfix: Comment Link import out, to fix pipeline crash

--- src/pages/IndexPage/index.js
+++ src/pages/IndexPage/index.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import './IndexStyle.scss';
 import MusicLibrary from './MusicLibrary';
 import Header from '../../components/Header';
