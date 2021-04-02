c0aaa9f440ea810a7ea9c407e28630475db7c142
Konsta Purtsi <konsta@identio.fi>
2021-04-02 14:21:54 +0300

    Improve modal.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,15 +5,36 @@ const Modal = ({ response, closeModal }) => {
   if (response.error) {
     return (
       <div className="Modal">
-        Lähetys epäonnistui :(
-        <Button onClick={closeModal}>Harmin paikka</Button>
+        <h1>Lähetys epäonnistui :(</h1>
+
+        Tämä on odottamaton virhe, jota ei pitäisi tapahtua. Syy virheelle voi olla:
+        <ul>
+          <li>
+            Syötit paljon tiedostoja ja muisti loppui kesken. Palvelulla on 1GB muistia käytössä,
+            tätä suurempi määrä tiedostoja ei toimi samassa suorituksessa.
+          </li>
+          <li>
+            Suoritus aikakatkaistiin. Suuri määrä tiedostoja voi aiheuttaa aikakatkaisun, sillä
+            palvelulla on 30 sekuntia suoritusaikaa. Tiedostojen lähettämiseen kulunut aika ei
+            vaikuta tähän, ja siten vika ei ole verkkonopeudessasi.
+          </li>
+        </ul>
+        <Button onClick={closeModal}>
+          Harmin paikka
+        </Button>
       </div>
     );
   }
 
+  const validationError = response.status === 'error';
+
   return (
     <div className="Modal">
-      <h1>Lähetys onnistui!</h1>
+      <h1>
+        {validationError
+          ? 'Musiikki on lähetetty tarkistettavaksi!'
+          : 'Lähetys onnistui!'}
+      </h1>
       <p>
         Musiikkikirjasto on vastaanottanut
         {response.album
@@ -22,7 +43,7 @@ const Modal = ({ response, closeModal }) => {
         }.
       </p>
       <p>
-        {response.status === 'error'
+        {validationError
           ? "Wappuradion musiikkitiimi tarkistaa tiedostot ennenkuin ne ovat soitettavissa lähetyksessä."
           : "Kappaleet ovat nyt Musiikkikirjastossa ja valmiina soitettavaksi lähetyksessä."}
       </p>
