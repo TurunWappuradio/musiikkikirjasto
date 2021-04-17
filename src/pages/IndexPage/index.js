4a65c6071cc006722527750aa00415a745874c3c
Konsta Purtsi <konsta@identio.fi>
2021-04-17 18:01:09 +0300

    Add header and content for release.

--- src/pages/IndexPage/index.js
+++ src/pages/IndexPage/index.js
@@ -1,4 +1,4 @@
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './IndexStyle.scss';
 import MusicLibrary from './MusicLibrary';
 import Header from '../../components/Header';
@@ -8,10 +8,10 @@ const IndexPage = () => (
     <Header title="Musiikkikirjasto"/>
     <div className="IndexContent">
       <p>Tervetuloa Turun Wappuradion musiikkikirjastoon!
-        Kirjaston kattavaa valikoimaa ei tällä hetkellä pysty kartuttamaan,
-        mutta ominaisuus lisätään tälle sivulle myöhemmin keväällä, malta siis vielä hetki.<br></br>
-        {/* Kirjasto on kattava, mutta mikäli etsimääsi kappaletta ei kuitenkaan löydy kirjastosta, 
-        voit lisätä laillisin keinoin hankitun kopion{' '}<Link to="/lisaa">musanlataussivulla</Link>.<br></br>*/}
+        {/* Kirjaston kattavaa valikoimaa ei tällä hetkellä pysty kartuttamaan,
+        mutta ominaisuus lisätään tälle sivulle myöhemmin keväällä, malta siis vielä hetki.<br></br> */}
+        Kirjasto on kattava, mutta mikäli etsimääsi kappaletta ei kuitenkaan löydy kirjastosta, 
+        voit lisätä laillisin keinoin hankitun kopion{' '}<Link to="/lisaa">musanlataussivulla</Link>.<br></br>
         Lisätietoja Turun Wappuradion musiikkikäytännöistä löydät{' '}
         <a target="_blank" rel="noreferrer" href="https://www.turunwappuradio.com/musiikki">täältä</a>.</p>
     </div>
