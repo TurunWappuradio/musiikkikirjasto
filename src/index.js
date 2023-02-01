c8e8087b93ef50b24ea754e3c400772576fdd272
Konsta Purtsi <konsta@arado.fi>
2023-02-01 16:32:23 +0200

    Update all the things to modern versions.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import dotenv from 'dotenv';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
