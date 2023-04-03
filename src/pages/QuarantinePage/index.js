34d8346e8865b184f80c13080ef8b8591040915a
Konsta Purtsi <konsta@arado.fi>
2023-04-03 17:34:28 +0300

    Adds get-quarantined query.

--- src/pages/QuarantinePage/index.js
+++ src/pages/QuarantinePage/index.js
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react';
+import { MantineProvider } from '@mantine/core';
+
 import Header from '../../components/Header';
 
+const LAMBDA_URL = "https://api.turunwappuradio.com/prod/music-lambda"
+
+const password = '123'
+
 const QuarantinePage = () => {
+  const [submissions, setSubmissions] = useState([]);
+
+  useEffect(() => {
+    const fn = async () => {
+      const res = await fetch(LAMBDA_URL, {
+        method: 'POST',
+        body: JSON.stringify({
+          operation: 'admin/get-quarantined',
+          password,
+        })
+      })
+      const data = await res.json();
+      setSubmissions(data.submissions);
+    }
+
+    fn();
+  }, [])
+
   return (
-    <>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
       <Header title="Karanteeni" />
-    </>
+    </MantineProvider>
   );
 };
 
