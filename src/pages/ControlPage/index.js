4f2148f4efe6d03f10e6512a42e90ba6962d80c1
emil mustonen <emppu.mustonen1@gmail.com>
2023-04-07 17:05:53 +0300

    runned prettier

--- src/pages/ControlPage/index.js
+++ src/pages/ControlPage/index.js
@@ -1,65 +1,61 @@
 import './ControlStyle.scss';
 import Header from '../../components/Header';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Table from './musicTable/index'
+import Table from './musicTable/index';
 import { isMobile } from 'react-device-detect';
-import tracklist from '../IndexPage/tracklist.json'
+import tracklist from '../IndexPage/tracklist.json';
 
 const LAMBDA_URL = process.env.REACT_APP_MUSIC_LAMBDA_URL;
-const prod_url = 'api.turunwappuradio.com/prod/music-lambda'
+const prod_url = 'api.turunwappuradio.com/prod/music-lambda';
 
 const ControlPage = () => {
-
-  const [songList, setSongList] = useState([])
-  const [songs, setSongs] = useState([])
+  const [songList, setSongList] = useState([]);
+  const [songs, setSongs] = useState([]);
 
   const update = () => {
     const newSongs = getRows(songList, songs.length);
     setSongs([...songs, ...newSongs]);
-  }
+  };
 
   const getRows = (songList, rowNum) => {
     return songList.slice(rowNum, rowNum + 100);
-  }
+  };
 
   useEffect(() => {
     setSongs(getRows(songList, 0));
   }, [songList]);
 
-useEffect(() => {
+  useEffect(() => {
     axios({
       method: 'post',
       url: LAMBDA_URL,
       data: {
         operation: 'admin/get-songs',
-        password: 'salaisuus'
-      }
-    })
-    .then(response => setSongs(response.data.songs))
-  
-  }, [])
+        password: 'salaisuus',
+      },
+    }).then((response) => setSongs(response.data.songs));
+  }, []);
 
-  console.log(songList)
+  console.log(songList);
 
   const tableHeaders = [
     {
-      Header: "Kappale",
-      accessor: "title"
+      Header: 'Kappale',
+      accessor: 'title',
     },
     {
-      Header: "Artisti",
-      accessor: "artist"
+      Header: 'Artisti',
+      accessor: 'artist',
     },
     {
-      Header: "Albumi",
-      accessor: "album"
+      Header: 'Albumi',
+      accessor: 'album',
     },
     {
-      Header: "Kesto",
-      accessor: "length"
-    }
-
+      Header: 'Kesto',
+      accessor: 'length',
+    },
   ];
   return (
     <>
@@ -68,10 +64,14 @@ useEffect(() => {
         <p>Tervetuloa hallintanäkymään :P</p>
       </div>
       <div>
-      <Table columns={tableHeaders} data={songs} update={update} hiddenColumns={isMobile ? ["length"] : []}>
-      </Table>
+        <Table
+          columns={tableHeaders}
+          data={songs}
+          update={update}
+          hiddenColumns={isMobile ? ['length'] : []}
+        ></Table>
       </div>
     </>
-  )
-}
+  );
+};
 export default ControlPage;
