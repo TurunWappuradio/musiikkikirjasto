fb78e70619a3db542086bc754a4f425bd050fc08
Konsta Purtsi <konsta@arado.fi>
2023-04-14 18:22:30 +0300

    Sort the files.

--- src/pages/QuarantinePage/FileTable.js
+++ src/pages/QuarantinePage/FileTable.js
@@ -1,7 +1,7 @@
 import { Title, Table } from '@mantine/core';
 
 const FileTable = ({ files }) => {
-  const rows = files.map((file) => (
+  const rows = files.sort(compareFile).map((file) => (
     <tr key={file.id}>
       <td>{file.filename}</td>
       <td>{file.filetype}</td>
@@ -24,6 +24,37 @@ const FileTable = ({ files }) => {
       </Table>
     </>
   );
+};
+
+const compareFile = (a, b) => {
+  // sort non audiofiles to the top.
+  if (!b?.metadata?.tracknumber) {
+    return 100;
+  }
+
+  const aDisc = parseInt(a?.metadata?.discnumber);
+  const bDisc = parseInt(b?.metadata?.discnumber);
+
+  // sort by discnubmer
+  if (aDisc < bDisc) {
+    return -1;
+  }
+  if (aDisc > bDisc) {
+    return 1;
+  }
+
+  const aTrack = parseInt(a?.metadata?.tracknumber);
+  const bTrack = parseInt(b?.metadata?.tracknumber);
+
+  // sort by tracknubmer
+  if (aTrack < bTrack) {
+    return -1;
+  }
+  if (aTrack > bTrack) {
+    return 1;
+  }
+
+  return 0;
 };
 
 export default FileTable;
